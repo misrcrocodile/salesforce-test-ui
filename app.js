880a3587ac568851035c2a2a6ac15aef79ea060f
anhnguyenyokogawa <150886669+anhnguyenyokogawa@users.noreply.github.com>
2024-03-20 23:04:07 +0900

    turning speed of loading page by change web from static to express

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Set static directory for serving HTML, CSS, JS files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ public/main.js
@@ -0,0 +1,403 @@
+var testList = [
+  {
+    name: "SALESFORCE ASSOCIATE",
+    value: "SalesforceAssociate",
+    testList: [
+      "ExamTopics.Associate.2023-09-30",
+      "Salesforceben.Associate.datamodel.q30",
+      "Salesforceben.Associate.ecosystem.q39",
+      "Salesforceben.Associate.navigation.q33",
+      "Salesforceben.Associate.reportDashboard.q18",
+      "Salesforceben.Associate.test1.q40",
+      "Salesforceben.Associate.test2.q40",
+      "Salesforceben.Associate.test3.q40",
+    ],
+  },
+  {
+    name: "CERTIFIED ADMINISTRATOR",
+    value: "SalesforceAdministrator",
+    testList: [
+      "Certlibrary.ADM-201.v2023-12-09.q1181.json",
+      "Salesforce.ADM-201.v2022-01-17.q124.json",
+      "Salesforce.ADM-201.v2022-02-01.q72.json",
+      "Salesforce.ADM-201.v2022-03-11.q103.json",
+      "Salesforce.ADM-201.v2022-05-02.q104.json",
+      "Salesforce.ADM-201.v2022-05-02.q74.json",
+      "Salesforce.ADM-201.v2022-05-04.q74.json",
+      "Salesforce.ADM-201.v2022-08-16.q84.json",
+      "Salesforce.ADM-201.v2022-08-25.q79.json",
+      "Salesforce.ADM-201.v2022-09-24.q73.json",
+      "Salesforce.ADM-201.v2023-03-15.q176.json",
+      "Salesforce.ADM-201.v2023-04-27.q85.json",
+      "Salesforce.ADM-201.v2023-10-02.q113.json",
+      "Salesforce.ADM-201.v2023-10-09.q118.json",
+      "Salesforce.ADM-201.v2023-11-02.q102.json",
+    ],
+  },
+  {
+    name: "ADVANCED ADMINISTRATOR",
+    value: "Salesforce.Advanced-Administrator",
+    testList: [
+      "Salesforce.Advanced-Administrator.v2020-07-25.q106",
+      "Salesforce.Advanced-Administrator.v2021-01-06.q115",
+      "Salesforce.Advanced-Administrator.v2021-06-08.q122",
+      "Salesforce.Advanced-Administrator.v2021-07-24.q60",
+      "Salesforce.Advanced-Administrator.v2021-11-26.q88",
+      "Salesforce.Advanced-Administrator.v2022-01-29.q61",
+      "Salesforce.Advanced-Administrator.v2022-04-08.q125",
+      "Salesforce.Advanced-Administrator.v2022-04-09.q102",
+      "Salesforce.Advanced-Administrator.v2022-05-21.q317",
+      "Salesforce.Advanced-Administrator.v2022-08-12.q152",
+      "Salesforce.Advanced-Administrator.v2022-08-22.q160",
+      "Salesforce.Advanced-Administrator.v2022-08-31.q166",
+      "Salesforce.Advanced-Administrator.v2023-01-12.q71",
+      "Salesforce.Advanced-Administrator.v2023-02-06.q62",
+      "Salesforce.Advanced-Administrator.v2023-06-10.q141",
+    ],
+  },
+  {
+    name: "PLATFORM DEVELOPER I",
+    value: "Salesforce.Platform-Developer-I",
+    testList: [
+      "Salesforce.PDI.v2020-02-07.q82",
+      "Salesforce.PDI.v2020-03-22.q100",
+      "Salesforce.PDI.v2021-02-07.q59",
+      "Salesforce.PDI.v2021-06-08.q54",
+      "Salesforce.PDI.v2021-06-20.q54",
+      "Salesforce.PDI.v2021-10-29.q72",
+      "Salesforce.PDI.v2021-11-04.q66",
+      "Salesforce.PDI.v2022-02-09.q66",
+      "Salesforce.PDI.v2022-03-30.q75",
+      "Salesforce.PDI.v2022-06-29.q254",
+      "Salesforce.PDI.v2022-08-06.q88",
+      "Salesforce.PDI.v2022-08-15.q90",
+      "Salesforce.PDI.v2022-08-28.q103",
+      "Salesforce.PDI.v2023-03-21.q230",
+      "Salesforce.PDI.v2023-07-24.q102",
+      "Salesforce.PDI.v2023-09-20.q68",
+    ],
+  },
+  {
+    name: "PLATFORM DEVELOPER II",
+    value: "Salesforce.Platform-Developer-II",
+    testList: [
+      "Salesforce.PDII.v2020-06-24.q45",
+      "Salesforce.PDII.v2020-10-27.q45",
+      "Salesforce.PDII.v2021-05-13.q99",
+      "Salesforce.PDII.v2021-07-16.q108",
+      "Salesforce.PDII.v2021-11-15.q113",
+      "Salesforce.PDII.v2021-11-18.q112",
+      "Salesforce.PDII.v2022-01-18.q113",
+      "Salesforce.PDII.v2022-03-29.q116",
+      "Salesforce.PDII.v2022-06-21.q138",
+      "Salesforce.PDII.v2022-07-07.q131",
+      "Salesforce.PDII.v2022-08-07.q138",
+      "Salesforce.PDII.v2023-02-10.q154",
+      "Salesforce.PDII.v2023-08-14.q106",
+      "Salesforce.PDII.v2023-09-09.q84",
+    ],
+  },
+  {
+    name: "PLATFORM APP BUILDER",
+    value: "Salesforce.Platform-App-Builder",
+    testList: [
+      "Certlibrary.Platform-App-Builder.v2023-11-29.q600",
+      "Salesforce.Platform-App-Builder.v2019-10-20.q99",
+      "Salesforce.Platform-App-Builder.v2019-10-25.q63",
+      "Salesforce.Platform-App-Builder.v2020-04-01.q60",
+      "Salesforce.Platform-App-Builder.v2020-08-09.q94",
+      "Salesforce.Platform-App-Builder.v2020-11-22.q105",
+      "Salesforce.Platform-App-Builder.v2021-01-05.q103",
+      "Salesforce.Platform-App-Builder.v2021-05-12.q109",
+      "Salesforce.Platform-App-Builder.v2021-08-03.q110",
+      "Salesforce.Platform-App-Builder.v2021-09-03.q110",
+      "Salesforce.Platform-App-Builder.v2022-01-19.q148",
+      "Salesforce.Platform-App-Builder.v2022-03-30.q110",
+      "Salesforce.Platform-App-Builder.v2022-05-13.q125",
+      "Salesforce.Platform-App-Builder.v2022-08-02.q159",
+      "Salesforce.Platform-App-Builder.v2022-08-31.q151",
+      "Salesforce.Platform-App-Builder.v2023-01-14.q90",
+      "Salesforce.Platform-App-Builder.v2023-03-06.q159",
+      "Salesforce.Platform-App-Builder.v2023-07-29.q129",
+    ],
+  },
+  {
+    name: "JAVASCRIPT DEVELOPER I",
+    value: "SalesforceJavascriptDeveloper1",
+    testList: [
+      "Salesforce.JsDevI.v2022-05-18.q146",
+      "Salesforce.JsDevI.v2022-07-10.q70",
+      "Salesforce.JsDevI.v2022-08-01.q60",
+      "Salesforce.JsDevI.v2023-02-25.q75",
+    ],
+  },
+  {
+    name: "MARKETING CLOUD ADMINISTRATOR",
+    value: "MarketingCloudAdministrator",
+    testList: [
+      "MarketingCloudAdmin.exam4training.q158",
+      "MarketingCloudAdmin.v2021-02-24.q37",
+      "MarketingCloudAdmin.v2021-12-21.q37",
+      "MarketingCloudAdmin.v2022-04-08.q47",
+      "MarketingCloudAdmin.v2022-05-04.q55",
+      "MarketingCloudAdmin.v2022-05-12.q62",
+    ],
+  },
+  {
+    name: "DATA ARCHITECT",
+    value: "Salesforce.Data-Architect",
+    testList: [
+      "Certlibrary.Data-Architect.v2023-11-09.q129",
+      "Salesforce.Data-Architect.v2023-01-20.q86",
+      "Salesforce.Data-Architect.v2023-03-15.q77",
+      "Salesforce.Data-Architect.v2023-09-04.q115",
+    ],
+  },
+  {
+    name: "SHARING & VISIBILITY ARCHITECT",
+    value: "Salesforce.Sharing-and-Visibility-Architect",
+    testList: [
+      "Certlibrary.Sharing-and-Visibility-Designer.v2023-11-27.q100",
+      "Salesforce.Sharing-and-Visibility-Architect.v2023-02-24.q75",
+      "Salesforce.Sharing-and-Visibility-Architect.v2023-05-26.q86",
+      "Salesforce.Sharing-and-Visibility-Designer.v2019-10-18.q50",
+      "Salesforce.Sharing-and-Visibility-Designer.v2020-07-19.q57",
+      "Salesforce.Sharing-and-Visibility-Designer.v2020-08-19.q78",
+      "Salesforce.Sharing-and-Visibility-Designer.v2020-09-16.q59",
+      "Salesforce.Sharing-and-Visibility-Designer.v2021-10-03.q72",
+      "Salesforce.Sharing-and-Visibility-Designer.v2022-03-29.q72",
+      "Salesforce.Sharing-and-Visibility-Designer.v2022-04-11.q147",
+      "Salesforce.Sharing-and-Visibility-Designer.v2023-06-24.q109",
+    ],
+  },
+
+  {
+    name: "IDENTITY & ACCESS MANAGEMENT ARCHITECT",
+    value: "Salesforce.Identity-and-Access-Management-Architect",
+    testList: [
+      "Salesforce.Identity-and-Access-Management-Architect.v2022-12-05.q87",
+      "Salesforce.Identity-and-Access-Management-Architect.v2023-06-17.q81",
+    ],
+  },
+  {
+    name: "B2C COMMERCE DEVELOPER",
+    value: "Salesforce.B2C-Commerce-Developer",
+    testList: [
+      "Salesforce.B2C-Commerce-Developer.v2020-08-20.q40",
+      "Salesforce.B2C-Commerce-Developer.v2021-04-18.q40",
+      "Salesforce.B2C-Commerce-Developer.v2022-05-20.q52",
+      "Salesforce.B2C-Commerce-Developer.v2022-05-28.q97",
+      "Salesforce.B2C-Commerce-Developer.v2022-07-21.q51",
+      "Salesforce.B2C-Commerce-Developer.v2022-11-30.q57",
+      "Salesforce.B2C-Commerce-Developer.v2023-01-05.q61",
+      "Salesforce.B2C-Commerce-Developer.v2023-02-22.q63",
+    ],
+  },
+];
+
+function fetchJSON(url) {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the JSON data
+      return data;
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+function initTestSelectList() {
+  var selectEl = document.getElementById("testType");
+  selectEl.innerHTML = "";
+  selectEl.appendChild(
+    createElementByText(
+      "<option value='' selected >Choose Test Type ...</option>"
+    )
+  );
+  for (var i = 0; i < testList.length; i++) {
+    selectEl.appendChild(
+      createElementByText(
+        `<option value="${testList[i].value}">${testList[i].name}</option>`
+      )
+    );
+  }
+}
+
+function onChangeTestType() {
+  var testTypeValue = document.querySelector("#testType").value;
+  var exQuestionEl = document.querySelector("#exampleQuestion");
+  var testListDetail = [];
+  if (!testTypeValue) return;
+  for (var i = 0; i < testList.length; i++) {
+    if (testList[i].value === testTypeValue) {
+      testListDetail = testList[i].testList;
+    }
+  }
+  // Sort by new to old
+  testListDetail.sort(function (a, b) {
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  });
+
+  exQuestionEl.innerHTML = "";
+  exQuestionEl.appendChild(
+    createElementByText(
+      "<option value='' selected >Choose Example Question...</option>"
+    )
+  );
+
+  for (var i = 0; i < testListDetail.length; i++) {
+    exQuestionEl.appendChild(
+      createElementByText(
+        `<option value="${testListDetail[i]}">${testListDetail[i]}</option>`
+      )
+    );
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  initTestSelectList();
+});
+
+window.testJSON = [];
+
+function getEscapeContent(str) {
+  var div = document.createElement("div");
+  div.innerText = str.trim();
+  return div.innerHTML;
+}
+function createElementByText(str) {
+  var div = document.createElement("div");
+  div.innerHTML = str.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes.
+  return div.firstChild;
+}
+
+function getAnswerArr(q, isShuffleOptions) {
+  var ans = "ABCDEFGHIJKLMNOPQ";
+  var result = q.answer.split(",");
+  for (var i = 0; i < result.length; i++) {
+    result[i] = ans.indexOf(result[i]);
+  }
+  var resultList = [];
+  for (var i = 0; i < q.options.length; i++) {
+    resultList.push({
+      value: q.options[i].substr(1, q.options[i].length),
+      isTrue: result.includes(i),
+    });
+  }
+  if (isShuffleOptions) {
+    resultList = shuffleArray(resultList);
+  }
+  for (var i = 0; i < resultList.length; i++) {
+    resultList[i].value = ans[i] + resultList[i].value;
+  }
+  return resultList;
+}
+
+function checkAnswer(id) {
+  var listAns = document.querySelectorAll(`#${id} .answer`);
+  for (var i = 0; i < listAns.length; i++) {
+    listAns[i].classList = ["answer"];
+  }
+}
+
+function createOptions(q, isShuffleOptions) {
+  var listOps = [];
+  var idEl = q.title.replaceAll(" ", "");
+  var checkType = q.answer.length > 1 ? "checkbox" : "radio";
+  var results = getAnswerArr(q, isShuffleOptions);
+  for (var i = 0; i < results.length; i++) {
+    var trueSpan = results[i].isTrue
+      ? `<span class="answer hidden">True</span>`
+      : "";
+    var str = `<div class="form-check">
+  <input class="form-check-input" type="${checkType}" value="" name="${idEl}" id="${idEl}${i}">
+  <label class="form-check-label" for="${idEl}${i}">
+    ${getEscapeContent(results[i].value)}${trueSpan}
+  </label>
+</div>`;
+    listOps.push(createElementByText(str));
+  }
+
+  return listOps;
+}
+
+function createQuestion(q, isShuffleQuestion) {
+  var idEl = q.title.replaceAll(" ", "");
+  var el = createElementByText(`<div class="question-container" id="${idEl}">
+  <label class="form-label"><strong>${q.title}:</strong> ${q.content.replaceAll(
+    "\n",
+    "<br>"
+  )}</label>
+
+  
+</div>`);
+  var opList = createOptions(q, isShuffleQuestion);
+  opList.push(
+    createElementByText(
+      `<button type="button" class="btn btn-primary" style="margin-top: 10px;" onClick="checkAnswer('${idEl}')">Show Answer</button>`
+    )
+  );
+  for (var i = 0; i < opList.length; i++) {
+    el.appendChild(opList[i]);
+  }
+  return el;
+}
+
+function getJsonData() {
+  var testTypeValue = document.querySelector("#testType").value;
+  var exQuestionValue = document.querySelector("#exampleQuestion").value;
+  if (testTypeValue === "") {
+    alert("Please select the test type!");
+    return [];
+  }
+  if (exQuestionValue === "") {
+    alert("Please select the example question!");
+    return [];
+  }
+  var testTypeValue = document.querySelector("#testType").value;
+  var testDetailName = document.querySelector("#exampleQuestion").value;
+  var url = `${window.location.origin}/data/${testTypeValue}/${testDetailName}.json`;
+
+  return fetchJSON(url).then((data) => {
+    console.log("data", data);
+    return data;
+  });
+}
+
+function loadQuestion() {
+  // Clear previous content
+  var isShuffle = document.querySelector("#isShuffle").checked;
+  var body = document.querySelector("#question-content");
+  body.innerHTML = "";
+  getJsonData().then((data) => {
+    if (isShuffle) {
+      data = shuffleArray(data);
+    }
+    for (var i = 0; i < data.length; i++) {
+      data[i].title = "Question " + (i + 1);
+    }
+    for (var i = 0; i < data.length; i++) {
+      body.appendChild(createQuestion(data[i], isShuffle));
+    }
+  });
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
