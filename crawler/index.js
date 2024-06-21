b5f1a60698a35d79603f31a15c77575565f52247
anhnguyenyokogawa <150886669+anhnguyenyokogawa@users.noreply.github.com>
2024-06-22 00:11:59 +0900

    fix content

--- crawler/index.js
+++ crawler/index.js
@@ -2,7 +2,40 @@ const curl = require("./curl");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const Promise = require("bluebird");
-var mainLinks = [{"name":"Salesforce.PDII.v2024-03-02.q90","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2024-03-02.q90.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2024-02-22.q73","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2024-02-22.q73.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2023-09-09.q84","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2023-09-09.q84.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2023-08-14.q106","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2023-08-14.q106.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2023-02-10.q154","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2023-02-10.q154.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2022-08-07.q138","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2022-08-07.q138.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2022-07-07.q131","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2022-07-07.q131.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2022-06-21.q138","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2022-06-21.q138.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2022-03-29.q116","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2022-03-29.q116.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2022-01-18.q113","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2022-01-18.q113.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2021-11-18.q112","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2021-11-18.q112.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2021-11-15.q113","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2021-11-15.q113.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2021-07-16.q108","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2021-07-16.q108.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2021-05-13.q99","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2021-05-13.q99.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2020-10-27.q45","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2020-10-27.q45.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"},{"name":"Salesforce.PDII.v2020-06-24.q45","url":"https://www-freecram-net.translate.goog/torrent/Salesforce.PDII.v2020-06-24.q45.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp"}];
+var mainLinks = [
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2023-06-24.q109",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2023-06-24.q109.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2022-04-11.q147",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2022-04-11.q147.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2022-03-29.q72",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2022-03-29.q72.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2021-10-03.q72",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2021-10-03.q72.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2020-09-16.q59",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2020-09-16.q59.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2020-08-19.q78",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2020-08-19.q78.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2020-07-19.q57",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2020-07-19.q57.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+  {
+    name: "Salesforce.Sharing-and-Visibility-Designer.v2019-10-18.q50",
+    url: "https://www-freecram-net.translate.goog/torrent/Salesforce.Sharing-and-Visibility-Designer.v2019-10-18.q50.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+  },
+];
 async function crawlLink(url) {
   let res = await curl(url);
   let $ = cheerio.load(res);
@@ -71,16 +104,12 @@ async function getData(mainLink) {
     fs.mkdirSync(dir);
   }
 
-  fs.writeFile(
-    "crawData/" + mainLink.name + ".json",
-    JSON.stringify(data),
-    (err) => {
-      if (err) {
-        console.error(err);
-      }
-      // file written successfully
+  fs.writeFile("crawData/" + mainLink.name + ".json", JSON.stringify(data), (err) => {
+    if (err) {
+      console.error(err);
     }
-  );
+    // file written successfully
+  });
 }
 async function init() {
   for (var i = 0; i < mainLinks.length; i++) {
@@ -90,17 +119,12 @@ async function init() {
 init();
 
 function convert() {
-  var an = Array.from(document.querySelectorAll("table.table a")).map(
-    (e) => e.href.split("https://www.freecram.net/torrent/")[1]
-  );
+  var an = Array.from(document.querySelectorAll("table.table a")).map((e) => e.href.split("https://www.freecram.net/torrent/")[1]);
   an = an.filter((e) => e != null);
   var bn = an.map((e) => {
     return {
       name: e.split(".html")[0],
-      url:
-        "https://www-freecram-net.translate.goog/torrent/" +
-        e +
-        "?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
+      url: "https://www-freecram-net.translate.goog/torrent/" + e + "?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp",
     };
   });
   return JSON.stringify(bn);
